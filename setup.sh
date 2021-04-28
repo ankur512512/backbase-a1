d4a8357e6bdb309b3c4fdbeccefe8ecabfeb92f3
ankur512512 <ankurgarg1203@gmail.com>
2021-04-28 20:25:46 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-echo "Log in to docker...\n\n"
+echo -e "Log in to docker...\n\n"
 docker login -u backbasedevops -p backbase@123
 
-echo "Building and tagging Docker images\n\n"
+echo -e "\n\nBuilding and tagging Docker images\n\n"
 docker build -f Dockerfile-tomcat . -t backbasedevops/tomcat
 docker build -f Dockerfile-nginx . -t backbasedevops/nginx
 
-echo "Pushing docker images to repo\n\n"
+echo -e "\n\nPushing docker images to repo\n\n"
 docker push backbasedevops/nginx
 docker push backbasedevops/tomcat
 
-echo "Installing k8s deployments\n\n"
+echo -e "\n\nInstalling k8s deployments\n\n"
 kubectl apply -f .
 
-echo "Waiting for 10 seconds for all the pods to come online...Stay with us\n\n"
+echo -e "\n\nWaiting for 10 seconds for all the pods to come online...Stay with us\n\n"
 
 sleep 10
 
 kubectl get pods
 
 
-echo "\n\n Everything is ready please test using the Steps listed in the document."
+echo -e "\n\n Everything is ready please test using the Steps listed in the document."
